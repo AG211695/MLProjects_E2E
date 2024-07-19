6cf7be0a1ffd79fe317cb8342d1653521a750e33
AG211695 <suzfall20@gmail.com>
2024-07-19 15:24:48 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+
+
+setup(
+name= 'mlProjects',
+version='0.0.1',
+author='Suz',
+author_email='suzfall20@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+
+)
